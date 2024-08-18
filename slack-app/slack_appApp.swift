fa79da2e06860af5588c9848ca87af6380ae08ce
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2024-08-18 22:27:44 +0900

    Initial Commit

---
+++ slack-app/slack_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  slack_appApp.swift
+//  slack-app
+//
+//  Created by Eiji Kudo on 2024/08/18.
+//
+
+import SwiftUI
+
+@main
+struct slack_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
